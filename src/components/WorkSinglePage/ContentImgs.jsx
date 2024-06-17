8ff7b35f37ad76fb3391dac8625ee1ac116c1b27
diogofbrito <diogobrito74@gmail.com>
2024-06-17 15:24:00 +0100

    feat: stand

--- src/components/WorkSinglePage/ContentImgs.jsx
+++ src/components/WorkSinglePage/ContentImgs.jsx
@@ -8,12 +8,12 @@ export function ContentImgs({ restMedia, lastMedia }) {
 			<div className='imgs__grid'>
 				{restMedia.map((media, index) => (
 					<div key={index} className='media-item'>
-						{media.endsWith('.mp4') ? <video autoPlay loop playsInline muted src={media}></video> : <img src={media} alt={`Image ${index}`} />}
+						{media.endsWith('.mp4') ? <video autoPlay loop playsInline muted src={media}></video> : <img src={media} alt={`Image ${index}`}  loading='lazy'/>}
 					</div>
 				))}
 			</div>
 			<div className='imgs__grid-last'>
-				{lastMedia && <div className='media-item'>{lastMedia.endsWith('.mp4') ? <video autoPlay loop playsInline muted src={lastMedia}></video> : <img src={lastMedia} alt='Last Media' />}</div>}
+				{lastMedia && <div className='media-item'>{lastMedia.endsWith('.mp4') ? <video autoPlay loop playsInline muted src={lastMedia}></video> : <img src={lastMedia} alt='Last Media' loading='lazy'/>}</div>}
 			</div>
 		</>
 	);
