58a27b3295435c4baa47df662cef4de1c51dd75b
diogofbrito <diogobrito74@gmail.com>
2024-07-02 19:03:08 +0100

    feat: mobile gallery

---
+++ src/components/GalleryMobile.jsx
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../contexts/AppContext';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectFade, Pagination, Autoplay } from 'swiper/modules';
+import LazyLoad from 'react-lazyload';
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+export function GalleryMobile() {
+	const { projects } = useContext(AppContext);
+
+	return (
+		<div className='galleryMobile'>
+			<Swiper
+				spaceBetween={30}
+				effect={'fade'}
+				autoplay={{
+					delay: 5000,
+					disableOnInteraction: false,
+				}}
+				modules={[EffectFade, Autoplay, Pagination]}
+			>
+				{projects.map((project, index) => (
+					<SwiperSlide key={`project_${index}`}>
+						<LazyLoad>
+							{project.videoMobile ? (
+								<video
+									src={project.videoMobile}
+									autoPlay
+									loop
+									muted
+									playsInline
+									onError={e => {
+										console.error(`Error loading video: ${project.videoMobile}`, e);
+									}}
+								></video>
+							) : (
+								<img src={project.imageMobile} alt={project.title} loading='lazy' />
+							)}
+						</LazyLoad>
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</div>
+	);
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { Gallery } from '../components/Gallery';
 import { About } from '../components/About';
 import { HeaderHome } from '../components/Header/HeaderHome';
+import { GalleryMobile } from '../components/GalleryMobile';
 
 export function Home() {
 	return (
@@ -10,6 +11,7 @@ export function Home() {
 			<div className='container'>
 				<Link to='/works'>
 					<Gallery />
+					<GalleryMobile />
 				</Link>
 
 				<div className='mobile__home'>
