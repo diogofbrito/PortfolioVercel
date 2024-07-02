87b786c4aad647947ba405ab9c4d85cacfa4fcaa
diogofbrito <diogobrito74@gmail.com>
2024-07-02 01:57:05 +0100

    feat: update

--- src/components/Header/HeaderHome.jsx
+++ src/components/Header/HeaderHome.jsx
@@ -3,17 +3,12 @@ import { NavLink } from 'react-router-dom';
 import { ThemeSwitch } from './ThemeSwitch.jsx';
 
 export function HeaderHome() {
-	const scrollToTop = () => {
-		window.scrollTo({
-			top: 0,
-			behavior: 'smooth',
-		});
-	};
+	
 
 	return (
 		<div className='navbarhome'>
 			<div className='navlinks'>
-				<NavLink to='/works' className='navlink' onClick={scrollToTop}>
+				<NavLink to='/works' className='navlink' >
 					Works
 				</NavLink>
 
