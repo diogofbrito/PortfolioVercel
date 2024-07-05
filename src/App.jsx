1a9cdd42bbfa69b5d875eff7ae6b3ae81aff5060
diogofbrito <diogobrito74@gmail.com>
2024-07-05 10:39:48 +0100

    feat: analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,20 @@ import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header/Header';
 import { AppProvider } from './contexts/AppContext';
 import { ViewProvider } from './contexts/ViewContext';
-
+import { Analytics } from '@vercel/analytics/react';
 import { FilterProvider } from './contexts/FilterContext';
 
 function App() {
 	return (
-		
-			<AppProvider>
-				<FilterProvider>
-					<ViewProvider>
-						<Header />
-						<Outlet />
-					</ViewProvider>
-				</FilterProvider>
-			</AppProvider>
-	
+		<AppProvider>
+			<FilterProvider>
+				<ViewProvider>
+					<Analytics />
+					<Header />
+					<Outlet />
+				</ViewProvider>
+			</FilterProvider>
+		</AppProvider>
 	);
 }
 
